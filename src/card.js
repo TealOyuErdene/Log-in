a27db220425db9e7581e165fc89aeb75e04a32c0
oyuerdene <teal.oyuerdene@gmail.com>
2023-01-02 18:44:23 +0800

    .

--- src/card.js
+++ src/card.js
@@ -1,29 +1,29 @@
 function Card(){
     return (
+		// <>  </>
         <div class="container">
-	<div class="front side">
-		<div class="content">
-			<h1>Hakkam Abdullah</h1>
-			<p>I am a graphic designer and art director. I am as well specialised in front end web design, user experience and creating identities. Throughout my career, I have worked with companies of all shapes and sizes that enriched my experience
-			</p>
+			<div class="front side">
+				<div class="content">
+					<h1>Hakkam Abdullah</h1>
+					<p>I am a graphic designer and art director. I am as well specialised in front end web design, user experience and creating identities. Throughout my career, I have worked with companies of all shapes and sizes that enriched my experience
+					</p>
+				</div>
+			</div>
+			<div class="back side">
+				<div class="content">
+					<h1>Contact Me</h1>
+					<form>
+						<label>Your Name :</label>
+						<input type="text"/>
+						<label>Your E-mail :</label>
+						<input type="text"/>
+						<label>Your message :</label>
+						<textarea></textarea>
+						<input type="submit" value="Done"/>
+					</form>
+				</div>
+			</div>
 		</div>
-	</div>
-	<div class="back side">
-		<div class="content">
-			<h1>Contact Me</h1>
-			<form>
-				<label>Your Name :</label>
-				<input type="text"/>
-				<label>Your E-mail :</label>
-				<input type="text"/>
-				<label>Your message :</label>
-				<textarea></textarea>
-				<input type="submit" value="Done"/>
-			</form>
-		</div>
-	</div>
-
-</div>
     )
 }
 
